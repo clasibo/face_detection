19a7e429c5eec7a18351c6589313ecfbd2c90c3b
Claudiu Siboroschi <122259713+clasibo@users.noreply.github.com>
2023-08-10 17:05:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+tsParticles.load("tsparticles", {
+  particles: {
+  	
+  	number: {
+		value: 250,
+			},
+
+	interactivity: {
+  		events: {
+  			onHover: {
+  				enable: true,
+  				mode: "repulse",
+  			}
+  		}
+  	},
+	color: {
+		value: '#000000'
+	},
+    
+      	move: {
+  		enable: true
+  	},
+
+  	links: {
+  		enable: true,
+  		color: {
+  			value: '#000000'
+  		}
+  	},
+ 
+  },
+ 
+});
+
+//https://particles.js.org/video.html
+//https://github.com/tsparticles/presets/tree/main/presets/links#readme
